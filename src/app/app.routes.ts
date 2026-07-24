5be777f8b2168c1a91758bcdc8c33cd318e53d1c
iagopedro <iagopedroltd19@gmail.com>
2026-07-23 22:05:11 -0300

    aula 04

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,15 @@ import { Livro } from './livro/livro';
 
 export const routes: Routes = [
   // TODO: Criar rota para livro
+  {
+    path: 'livro',
+    component: Livro
+  }
+,
+  // TODO: Criar rota para página inicial
+  {
+    path: '',
+    redirectTo: '/livro',
+    pathMatch: 'full'
+  }
 ];
